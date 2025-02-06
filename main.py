452767a02c69d286e30f9c519439192e21708bfb
Lorenzo Soricone <lorenzo.soricone02@gmail.com>
2025-02-06 18:16:19 +0100

    Aggiunto file .gitignore, creata classe DataPreprocessing, dove ho migliorato il processo di pulizia dei dati risolvendo alcuni errori logici e ho modificato il main.py così che ha all'interno solo le istruzioni per processare il dataset

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
-import pandas as pd
-import numpy as np
+from DataPreprocessing import PreprocessingDataset
 
-#  Caricamento del dataset
-file_path = input("inserire path dataset ")# Esempio: /Users/claudia/Desktop/Data/version_1.csv
-dataset = pd.read_csv(file_path)
-
-# Conversione a numerico (trasforma stringhe in NaN se non convertibili)
-dataset = dataset.apply(pd.to_numeric, errors='coerce')
-
-# Rimuovi righe con valori mancanti
-dataset = dataset.dropna()
-
-# Sostituire i NaN con la media:
-dataset = dataset.fillna(dataset.mean(numeric_only=True))
-
-# Controllo valori mancanti dopo la pulizia
-print(dataset.isnull().sum())
-
-# Separazione delle features (X) e della variabile target (y)
-X = dataset.drop('classtype_v1', axis=1)
-y = dataset['classtype_v1']
-
-#  Normalizzazione Min-Max manuale
-X_min = X.min()
-X_max = X.max()
-
-X_normalized = (X - X_min) / (X_max - X_min)
-
-# Visualizzazione dei dati normalizzati come DataFrame (opzionale)
-dataset_normalized = pd.DataFrame(X_normalized, columns=X.columns)
-print(dataset_normalized.head())
-
-#  Controllo della variabile target
-print(y)
-
-
-
+if __name__ == "__main__":
+    # Creiamo un'istanza della classe
+    preprocessing = PreprocessingDataset()
+    
+    # Carico dataset
+    dataset_caricato = preprocessing.carica_dataset()
+    
+    # Pulisco il dataset
+    dataset_pulito = preprocessing.pulisci_dataset()
+    
+    # Separo le features e il target
+    x, y = preprocessing.separa_features_e_target(dataset_pulito)
+    
+    # Normalizzo le features
+    x_normalizzato = preprocessing.normalizza_features()
+    
+    print("\nLe prime 5 righe delle features normalizzate:\n")
+    print(x_normalizzato.head())
