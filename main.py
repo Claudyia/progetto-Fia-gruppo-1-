da857f8506a4fa0f3d4d56158fbe322d77e7e5ba
Lorenzo Soricone <lorenzo.soricone02@gmail.com>
2025-02-09 13:22:50 +0100

    Aggiornato il main con alcune funzionalità e la capacità di effettuare il KNN solo con K-Fold-Cross-validation

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
 from DataPreprocessing import PreprocessingDataset
+from Kfold_CrossValidation import KfoldCrossValidationKNN
 
 if __name__ == "__main__":
     # Creiamo un'istanza della classe
     preprocessing = PreprocessingDataset()
-    
     # Carico dataset
     dataset_caricato = preprocessing.carica_dataset()
     
     # Pulisco il dataset
-    dataset_pulito = preprocessing.pulisci_dataset()
-    
-    # Separo le features e il target
-    x, y = preprocessing.separa_features_e_target(dataset_pulito)
+    X, y = preprocessing.pulisci_dataset()
     
     # Normalizzo le features
     x_normalizzato = preprocessing.normalizza_features()
-    
-    print("\nLe prime 5 righe delle features normalizzate:\n")
-    print(x_normalizzato.head())
+
+    if (len(x_normalizzato) != len(y)):
+        raise ValueError("Errore: il numero di righe delle features e del target non coincide!")
+    else:
+        print("\nIl numero di righe delle features e del target coincide!\n")
+
+    if (len(x_normalizzato) == 0):
+        raise ValueError("Errore: il numero di righe delle features è zero!")
+    else:
+        metodo = int(input(
+        "Scegliere il metodo (digitando '1', '2' o '3') che si vuole usare per la KNN:\n"
+        "1. Holdout (Non Disponibile)\n"
+        "2. K-fold Cross Validation\n"
+        "3. Random Subsampling (Non Disponibile)\n"
+        ))
+
+
+    if (metodo == 2):
+        k = int(input("Inserisci il numero di vicini (k) per l'algoritmo KNN: "))   
+        K = int(input("Inserisci il numero di fold per la cross-validation: "))
+        kfold = KfoldCrossValidationKNN(k, K)
+
+        # Valutiamo il modello
+        accuracy, error, sensitivity, specificity, geometric_mean, model_accuracy, model_error, model_sensitivity, model_specificity, model_geometric_mean, confusion_matrix = kfold.evaluate(x_normalizzato, y)
+
+        print(f"\nAccuracy: {model_accuracy*100}%")
+        print(f"Error: {model_error*100}%")
+        print(f"Sensitivity: {model_sensitivity*100}%")
+        print(f"Specificity: {model_specificity*100}%")
+        print(f"Geometric Mean: {model_geometric_mean*100}%")
+
+        for idx, matrix in enumerate(confusion_matrix):
+            TN, FP = int(matrix[0, 0]), int(matrix[0, 1])
+            FN, TP = int(matrix[1, 0]), int(matrix[1, 1])
+            
+            print(f"Confusion Matrix {idx+1}:")
+            print(f"TN: {TN}, FP: {FP}")
+            print(f"FN: {FN}, TP: {TP}\n")
+    else:
+        print("Metodo non ancora implementato!")
