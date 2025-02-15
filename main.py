505380ec28b73b4873c489da41e2f4581cb3911f
Lorenzo Soricone <lorenzo.soricone02@gmail.com>
2025-02-15 17:14:43 +0100

    Aggiunta la possibilità del random subsampling come metodo di validazione del modello

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from DataPreprocessing import PreprocessingDataset
 from knn import KNN
 from Kfold_CrossValidation import KfoldCrossValidationKNN
 from HoldOut import HoldOut
+from classe_random_subsampling import RandomSubsampling
 
 if __name__ == "__main__":
     # Creiamo un'istanza della classe"
@@ -27,12 +28,13 @@ if __name__ == "__main__":
         "Scegliere il metodo (digitando '1', '2' o '3') che si vuole usare per la KNN:\n"
         "1. Holdout\n"
         "2. K-fold Cross Validation\n"
-        "3. Random Subsampling (Non Disponibile)\n"
+        "3. Random Subsampling\n"
         ))
 
     if metodo == 1:
         k = int(input("Inserisci il numero di vicini (k) per l'algoritmo KNN: "))
-        holdout = HoldOut(k)
+        dim_test_set = float(input("Inserisci la dimensione del test set: "))
+        holdout = HoldOut(k, dim_test_set)
 
         # Valutiamo il modello
         accuracy, error, sensitivity, specificity, geometric_mean, confusion_matrix = holdout.evaluate(x_normalizzato, y)
@@ -65,5 +67,28 @@ if __name__ == "__main__":
             print(f"Confusion Matrix {idx+1}:")
             print(f"TN: {TN}, FP: {FP}")
             print(f"FN: {FN}, TP: {TP}\n")
-    else:
-        print("Metodo non ancora implementato!")
+    elif metodo == 3:
+        k = int(input("Inserisci il numero di vicini (k) per l'algoritmo KNN: "))
+        dim_test_set = float(input("Inserisci la dimensione del test set: "))
+        K = int(input("Inserisci il numero di iterazione per il random-subsampling: "))
+        
+        random_subsampling = RandomSubsampling(k, K, dim_test_set)
+        accuracy_media, error_rate_media, sensitivity_media, specificity_media, geometric_mean_media, confusion_matrix_iterazione = random_subsampling.evaluate(x_normalizzato, y)
+
+        print(f"\nAccuracy: {accuracy_media*100}%")
+        print(f"Error Rate: {error_rate_media*100}%")
+        print(f"Sensitivity: {sensitivity_media*100}%")
+        print(f"Specificity: {specificity_media*100}%")
+        print(f"Geometric Mean: {geometric_mean_media*100}%")
+
+        for idx, matrix in enumerate(confusion_matrix_iterazione):
+            TN, FP = int(matrix[0, 0]), int(matrix[0, 1])
+            FN, TP = int(matrix[1, 0]), int(matrix[1, 1])
+            
+            print(f"Confusion Matrix {idx+1}:")
+            print(f"TN: {TN}, FP: {FP}")
+            print(f"FN: {FN}, TP: {TP}\n")
+
+    else: 
+        raise ValueError("Errore: metodo non disponibile!")
+
