a7ca60d1ddb136cbb3e4de780b27ab573af1c579
Claudyia <claudia.cordova373@gmail.com>
2025-02-21 16:25:43 +0100

    aggiornato test_calcolo_metriche.py

--- test_calcolo_metriche.py
+++ test_calcolo_metriche.py
@@ -15,57 +15,58 @@ class TestMetriche(unittest.TestCase):
     def test_accuracy(self):
         """Testa il calcolo dell'accuratezza."""
         accuracy = Accuracy().calcola(self.y_test, self.y_pred)
-        expected_accuracy = np.sum(self.y_pred == self.y_test) / len(self.y_test)
+        expected_accuracy = 0.75
         self.assertAlmostEqual(accuracy, expected_accuracy, places=4)
 
     def test_error(self):
         """Testa il calcolo dell'errore."""
         error = Error().calcola(self.y_test, self.y_pred)
-        expected_error = np.sum(self.y_pred != self.y_test) / len(self.y_test)
+        expected_error = 0.25
         self.assertAlmostEqual(error, expected_error, places=4)
 
     def test_sensitivity(self):
         """Testa il calcolo della sensitivity."""
         sensitivity = Sensitivity().calcola(self.y_test, self.y_pred)
-        TP = np.sum((self.y_pred == 2) & (self.y_test == 2))
-        FN = np.sum((self.y_pred == 4) & (self.y_test == 2))
-        expected_sensitivity = TP / (TP + FN)
+        expected_sensitivity = 0.75
         self.assertAlmostEqual(sensitivity, expected_sensitivity, places=4)
 
     def test_specificity(self):
         """Testa il calcolo della specificità."""
         specificity = Specificity().calcola(self.y_test, self.y_pred)
-        TN = np.sum((self.y_pred == 4) & (self.y_test == 4))
-        FP = np.sum((self.y_pred == 2) & (self.y_test == 4))
-        expected_specificity = TN / (TN + FP)
+        expected_specificity = 0.75
         self.assertAlmostEqual(specificity, expected_specificity, places=4)
 
     def test_geometric_mean(self):
         """Testa il calcolo della media geometrica."""
         geometric_mean = GeometricMean().calcola(self.y_test, self.y_pred)
-        sensitivity = Sensitivity().calcola(self.y_test, self.y_pred)
-        specificity = Specificity().calcola(self.y_test, self.y_pred)
-        expected_geometric_mean = np.sqrt(sensitivity * specificity) if sensitivity * specificity > 0 else 0
+        expected_geometric_mean = 0.75
         self.assertAlmostEqual(geometric_mean, expected_geometric_mean, places=4)
 
     def test_auc(self):
         """Testa il calcolo dell'AUC."""
-        auc = AUC().calcola(self.y_test, self.y_scores, self.knn)
-        tprs, fprs = self.knn.ROC_curve(self.y_test, self.y_scores)
-        expected_auc = np.trapz(tprs, fprs)
+        auc, roc_data = AUC().calcola(self.y_test, self.y_scores, self.knn)
+        tprs_expected = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.5, 0.75, 1.0])
+        fprs_expected = np.array([0.0, 0.0, 0.25, 0.5, 0.75, 1.0, 1.0, 1.0, 1.0, 1.0])
+        expected_auc = np.trapz(tprs_expected, fprs_expected)
+        expected_roc_data = (tprs_expected, fprs_expected)
+        np.testing.assert_array_almost_equal(roc_data, expected_roc_data, decimal=4)
         self.assertAlmostEqual(auc, expected_auc, places=4)
 
     def test_all_metrics(self):
         """Testa il calcolo di tutte le metriche."""
-        all_metrics = AllMetrics().calcola(self.y_test, self.y_pred, self.y_scores, self.knn)
-        accuracy = Accuracy().calcola(self.y_test, self.y_pred)
-        error = Error().calcola(self.y_test, self.y_pred)
-        sensitivity = Sensitivity().calcola(self.y_test, self.y_pred)
-        specificity = Specificity().calcola(self.y_test, self.y_pred)
-        geometric_mean = GeometricMean().calcola(self.y_test, self.y_pred)
-        auc = AUC().calcola(self.y_test, self.y_scores, self.knn)
+        all_metrics, roc_data = AllMetrics().calcola(self.y_test, self.y_pred, self.y_scores, self.knn)
+        accuracy = 0.75
+        error = 0.25
+        sensitivity = 0.75
+        specificity = 0.75
+        geometric_mean = 0.75
+        tprs_expected = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.5, 0.75, 1.0])
+        fprs_expected = np.array([0.0, 0.0, 0.25, 0.5, 0.75, 1.0, 1.0, 1.0, 1.0, 1.0])
+        roc_expected = (tprs_expected, fprs_expected)
+        auc = np.trapz(tprs_expected, fprs_expected)
         expected_all_metrics = np.array([accuracy, error, sensitivity, specificity, geometric_mean, auc])
         np.testing.assert_array_almost_equal(all_metrics, expected_all_metrics, decimal=4)
+        np.testing.assert_array_almost_equal(roc_data, roc_expected, decimal=4)
 
 if __name__ == '__main__':
     unittest.main()
